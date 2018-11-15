519d51c05e7957404bf2acd8b64beb2ea1a2042c
cifulton <cfulton@dunwoody.edu>
2018-11-15 09:26:13 -0600

    adding changes

--- js_example_one/calc.js
+++ js_example_one/calc.js
@@ -3,19 +3,24 @@
     This script will get the input from two text boxes 
     and calculate the total value.
 */
+
+//calcTotal(); //Involkes or call function
 //Specify a function that calculates total
 function calcTotal(){
     //Declaring variables
-    var numberOne, numberTwo, total;
+    var numberOne, numberTwo, total, totalMulti;
 
     //Two statements that gathers the value for each textbox
     numberOne = document.getElementById("box_one").value;
-    numberTwo = document.getElementById("box_two").value;
+    numberTwo =  document.getElementById("box_two").value;
 
     //Simply calculation adding the two numbers
     total = parseInt(numberOne) + parseInt(numberTwo);
 
-    document.getElementById('output').innerHTML = total;
+    //Mulitply the two numbers
+    totalMulti = parseInt(numberOne) * parseInt(numberTwo);
+
+    document.getElementById('output').innerHTML = "The numbers added: " + total + " The numbers multiplied: "+ totalMulti;
     //Using console.log to test and make sure variables are assigned
     console.log("First value is: " + numberOne + " Second value is: " + numberTwo);
 }
