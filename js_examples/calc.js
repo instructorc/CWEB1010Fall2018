860c17a0bdb0197daaeba507fc59afe3aaad3819
cifulton <cfulton@dunwoody.edu>
2018-11-15 13:25:38 -0600

    adding a few changes

--- js_examples/calc.js
+++ js_examples/calc.js
@@ -2,14 +2,19 @@
     Author: C.Fulton
     This a script that calculates the total based on user input
 */
+
+
+
+//Defining
 function calcTotal(){
     //Declare variables 
     var number_one, number_two, total;
 
     //This targets the input boxes and get the users value
-    number_one = document.getElementById('box_one').value;
+    number_one = document.getElementbyId('box_one').value;
     number_two = document.getElementById('box_two').value;
 
+    //Troubleshooting to make sure variables are being assigned appropriately
     console.log("First value: " + number_one + " Second Value: "+ number_two);
 
     //Calculate Total 
